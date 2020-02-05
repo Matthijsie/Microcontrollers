2b66c57396d6b9451698f0f56a596e48d5191be6
Matt <mp.westra@student.avans.nl>
2020-02-05 13:50:36 +0100

    Added B1

---
+++ Projects/Week2_B2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Week2_B2.c
+ *
+ * Created: 5-2-2020 13:49:58
+ * Author : matth
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
