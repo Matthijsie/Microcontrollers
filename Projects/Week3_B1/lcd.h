edc088de413f982472b2e40c312a041728391cd6
Matt <mp.westra@student.avans.nl>
2020-03-04 13:32:31 +0100

    Added B1

---
+++ Projects/Week3_B1/lcd.h
@@ -0,0 +1,14 @@
+/*
+ * lcd.h
+ *
+ * Created: 19-2-2020 11:59:12
+ *  Author: matth
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void lcd_init(void);
+void lcd_display_text(char *str);
+void lcd_set_cursor(int position);#endif  
